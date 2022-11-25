f31ddb9814913303c2e578f60012b9d3de8de18c
rishika251 <rishikasadineni@gmail.com>
2022-11-25 13:17:33 +0530

    Added:ability to start game

---
+++ GamblingSimulator.java
@@ -0,0 +1,13 @@
+package com.bl.workshop;
+
+public class GamblingSimulator {
+	 public static final  int INITIAL_STAKE = 100;
+	    public static final int STAKE_BET = 1;
+
+	    public static void main(String[] args)
+	    {
+	        System.out.println("Welcome To Gambler Simulation");
+	        System.out.println("Initial Stake is " +INITIAL_STAKE +"$");
+	        System.out.println("Stake Bet is " +STAKE_BET+"$");
+	    }
+}
