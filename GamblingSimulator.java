8b14fb549b2e8f1e5191344498d6a63d46ceea71
rishika251 <rishikasadineni@gmail.com>
2022-11-25 13:35:35 +0530

    Added:check Each month won or lost

--- GamblingSimulator.java
+++ GamblingSimulator.java
@@ -1,40 +1,46 @@
 package com.bl.workshop;
 
 public class GamblingSimulator {
-	public static final  int INITIAL_STAKE = 100;
-    public static final int STAKE_BET = 1;
-    public static int stake = 0;
-    public static final int STAKE=100;
-    public static final int BET=1;
+	 public static final int STAKE=100;
+	    public static final int BET=1;
+	    public static final int PERCENTAGE_50_STAKE=(STAKE/100)*50;
+	    public static final int HIGH_LIMIT=STAKE+PERCENTAGE_50_STAKE;
+	    public static final int LOW_LIMIT=STAKE-PERCENTAGE_50_STAKE;
+	    public static final int START_DAY=1;
+	    public static final int MAX_DAY=20;
+	    public static final int DAYS_IN_MONTH=30;
+	    public static final int STAKES_PER_DAY=50;
+	    public static final int START_DOLLARS=0;
 
-    public static void winOrLose()
-    {
-        if(Math.random()<0.5) {
-            stake++;
-            System.out.println("Win");;
-        }
-        else {
-            stake --;
-            System.out.println("Lose");
-    
-        int totalStake=STAKE;
+	    public static void main(String[] args) {
+	        System.out.println("Welcome to the Gambler Game");
+	        int days = START_DAY;
+	        int totalDollars = START_DOLLARS;
+	        for (days = START_DAY; days <= MAX_DAY; days++)
 
-        System.out.println("Welcome to the Gambler Game");
+	        for (days = START_DAY; days <= DAYS_IN_MONTH; days++)
+	        {
+	            int totalStake = STAKE;
 
-        int game=(int) (Math.random() * 10) % 2;
-        if(game==1){
-            totalStake=totalStake+BET;
-            System.out.println("Gambler won the bet and have total stake:"  +totalStake);
-        }else{
-            totalStake=totalStake-BET;
-           System.out.println("Gambler lost the bet and have total stake:" +totalStake);
-        }
-        }
-    }
-    public static void main(String[] args)
-    {
-        System.out.println("Welcome To Gambler Simulation");
-        System.out.println("Initial Stake is " +INITIAL_STAKE +"$");
-        System.out.println("Stake Bet is " +STAKE_BET+"$");
-    }
-}
+
+	            
+	                totalDollars += STAKES_PER_DAY;
+	                System.out.println("Gambler won by " +totalDollars+" dollars on day "+days);
+	           
+	            }
+	            
+	                totalDollars -= STAKES_PER_DAY;
+	                System.out.println("Gambler lost by " +totalDollars+" dollars on day "+days);
+	            }
+	    {
+	
+
+	        int totalDollars = 0;
+			if(totalDollars>START_DOLLARS)
+	        {
+	            System.out.println("Gambler won " +totalDollars);
+	        } else {
+	            System.out.println("Gambler lost " + totalDollars);
+	        }
+	    }
+	}
